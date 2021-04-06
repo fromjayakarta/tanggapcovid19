98b606ed492a9639c5c20839da65bf9822325783
Slamet Darmawan <fromjayakarta@gmail.com>
2021-04-06 11:49:55 +0700

    adding plugins theme-my-login and peters-login-redirect

--- wp-content/themes/one-page-express/functions.php
+++ wp-content/themes/one-page-express/functions.php
@@ -41,3 +41,32 @@ function one_page_express_php_version_notice() {
 if( version_compare(phpversion(), OPE_THEME_REQUIRED_PHP_VERSION, '>=')){
     require_once get_template_directory() . "/inc/functions.php";
 }
+
+add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
+function add_login_logout_link($items, $args) {
+        ob_start();
+        wp_loginout('index.php');
+        $loginoutlink = ob_get_contents();
+        ob_end_clean();
+        $items .= '<li>'. $loginoutlink .'</li>';
+    return $items;
+}
+
+add_filter( 'admin_bar_menu', 'replace_wordpress_howdy', 25 );
+function replace_wordpress_howdy( $wp_admin_bar ) {
+$my_account = $wp_admin_bar->get_node('my-account');
+$newtext = str_replace( 'Howdy,', 'Welcome,', $my_account->title );
+$wp_admin_bar->add_node( array(
+'id' => 'my-account',
+'title' => $newtext,
+) );
+}
+
+function get_user_role() {
+	global $current_user;
+
+	$user_roles = $current_user->roles;
+	$user_role = array_shift($user_roles);
+
+	return $user_role;
+}
